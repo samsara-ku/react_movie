472bbc941f237af17633fa802ba772456dfece39
yunhoe <yunhoe@deepnatural.ai>
2020-05-30 16:44:56 +0900

    Done with chapter 5

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Proptypes from 'prop-types';
+import "./Movie.css"
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
     return (
-        <h5>{title}</h5>
+        <div className="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movies_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="movie_genres">{genres.map((genre, index) => (
+                    <li key={index} className="genres_genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie_summary">{summary.slice(0, 180)}...</p>
+            </div>
+        </div>
     )
 }
 
@@ -12,7 +24,8 @@ Movie.prototype = {
     year: Proptypes.number.isRequired,
     title: Proptypes.string.isRequired,
     summary: Proptypes.string.isRequired,
-    poster: Proptypes.string.isRequired
+    poster: Proptypes.string.isRequired,
+    genres: Proptypes.arrayOf(Proptypes.string).isRequired
 }
 
 export default Movie;
